2882fc1b35ec3a6dd5c8158a975cf9997ba2a924
StannisGr <bvc344@gmail.com>
2022-03-21 16:30:12 +0300

    debug setup

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt', 'r') as file:
-    install_requires = [x for x in file.read().splitlines()]
-
-with open('README.md', 'r') as file:
-    description = file.read()
-
 version = '0.0.1'
 
 setup(
     name='yaschedule',
     version=version,
-    description=description,
+    description='Lib for getting schedule data from Yandex Schedule API',
     url='https://github.com/StannisGr/yaschedule',
     author='StannisGr',
+    author_email='bvc344@gmail.com',
     license='Apache License 2.0',
     platforms=['any'],
     keywords='yandex, schedule, api',
     packages=find_packages(where='yaschedule'),
-    install_requires=install_requires,
+    install_requires=[
+    'certifi>=2021.10.8',
+    'charset-normalizer>=2.0.12',
+    'idna>=3.3',
+    'requests>=2.27.1',
+    'urllib3>=1.26.9',
+    ],
 )
