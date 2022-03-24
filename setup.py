a752c778d29a23d8328435d344916600420c2ae1
StannisGr <bvc344@gmail.com>
2022-03-24 15:11:29 +0300

    modify README.md, setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
-version = '0.0.3'
+version = '0.0.3.1'
+
+directory = Path(__file__).parent
+
+with open(Path(directory, 'README.md'), 'r') as file:
+    if file.readable():
+        long_description = file.read()
+    else:
+        long_description = ''
+
 
 setup(
     name='yaschedule',
     version=version,
     description='Lib for getting schedule data from Yandex Schedule API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/StannisGr/yaschedule',
     author='StannisGr',
     author_email='bvc344@gmail.com',
     license='Apache License 2.0',
     platforms=['any'],
-    keywords='yandex, schedule, api',
-    packages=find_packages(exclude='tests'),
+    keywords='yandex, schedule, api, rasp',
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
     install_requires=[
     'certifi>=2021.10.8',
     'charset-normalizer>=2.0.12',
